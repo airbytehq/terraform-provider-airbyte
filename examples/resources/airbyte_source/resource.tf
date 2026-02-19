33232df414f5afbebca04bfabbf5d3adf179126d
aaronsteers <18150651+aaronsteers@users.noreply.github.com>
2026-02-19 00:59:33 +0000

    chore: regenerate SDK with Speakeasy

--- examples/resources/airbyte_source/resource.tf
+++ examples/resources/airbyte_source/resource.tf
@@ -1,59 +1,30 @@
-# Using the connector_configuration data source (recommended)
-data "airbyte_connector_configuration" "postgres" {
-  connector_name    = "source-postgres"
-  connector_version = "3.6.28"
-
-  configuration = {
-    host     = "db.example.com"
-    port     = 5432
-    database = "mydb"
-    schemas  = ["public"]
-    ssl_mode = { mode = "prefer" }
-  }
-
-  configuration_secrets = {
-    username = var.pg_user
-    password = var.pg_password
-  }
-}
-
-resource "airbyte_source" "postgres" {
-  name          = "Postgres Production"
-  workspace_id  = var.workspace_id
-  definition_id = data.airbyte_connector_configuration.postgres.definition_id
-  configuration = data.airbyte_connector_configuration.postgres.configuration_json
-}
-
-# Using inline JSON configuration
-resource "airbyte_source" "github" {
-  name          = "GitHub"
-  workspace_id  = var.workspace_id
-  definition_id = "ef69ef6e-aa7f-4af1-a01d-ef775033524e"
-
-  configuration = jsonencode({
-    repositories = ["airbytehq/airbyte"]
-    credentials = {
-      option_title          = "PAT Credentials"
-      personal_access_token = var.github_pat
-    }
-  })
-}
-
-# Migrating from a typed resource using 'moved' (Terraform >= 1.8)
-moved {
-  from = airbyte_source_pardot.my_source
-  to   = airbyte_source.my_source
-}
-
 resource "airbyte_source" "my_source" {
-  name          = "Pardot"
-  workspace_id  = var.workspace_id
-  definition_id = "5e6175e5-68e1-4c17-bff9-56103bbb0d80"
-
-  configuration = jsonencode({
-    client_id               = var.pardot_client_id
-    client_secret           = var.pardot_client_secret
-    refresh_token           = var.pardot_refresh_token
-    pardot_business_unit_id = "0Uv5g0000008OT2CAM"
-  })
+  configuration = "{ \"see\": \"documentation\" }"
+  definition_id = "e735c764-352e-4392-b04a-4052108601be"
+  name          = "...my_name..."
+  resource_allocation = {
+    default = {
+      cpu_limit                 = "...my_cpu_limit..."
+      cpu_request               = "...my_cpu_request..."
+      ephemeral_storage_limit   = "...my_ephemeral_storage_limit..."
+      ephemeral_storage_request = "...my_ephemeral_storage_request..."
+      memory_limit              = "...my_memory_limit..."
+      memory_request            = "...my_memory_request..."
+    }
+    job_specific = [
+      {
+        job_type = "check_connection"
+        resource_requirements = {
+          cpu_limit                 = "...my_cpu_limit..."
+          cpu_request               = "...my_cpu_request..."
+          ephemeral_storage_limit   = "...my_ephemeral_storage_limit..."
+          ephemeral_storage_request = "...my_ephemeral_storage_request..."
+          memory_limit              = "...my_memory_limit..."
+          memory_request            = "...my_memory_request..."
+        }
+      }
+    ]
+  }
+  secret_id    = "...my_secret_id..."
+  workspace_id = "56bc2b36-2925-428d-a458-4395acf723c5"
 }
