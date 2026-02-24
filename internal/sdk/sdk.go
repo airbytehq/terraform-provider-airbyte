758e136b1fccb8ed94f69cb21541f60fce0bf6eb
aaronsteers <18150651+aaronsteers@users.noreply.github.com>
2026-02-24 03:06:07 +0000

    chore: regenerate SDK with Speakeasy

--- internal/sdk/sdk.go
+++ internal/sdk/sdk.go
@@ -152,9 +152,9 @@ func WithTimeout(timeout time.Duration) SDKOption {
 // New creates a new instance of the SDK with the provided options
 func New(opts ...SDKOption) *SDK {
 	sdk := &SDK{
-		SDKVersion: "1.0.0-rc8",
+		SDKVersion: "1.0.0",
 		sdkConfiguration: config.SDKConfiguration{
-			UserAgent:  "speakeasy-sdk/terraform 1.0.0-rc8 2.836.5 1.0.0 github.com/airbytehq/terraform-provider-airbyte/internal/sdk",
+			UserAgent:  "speakeasy-sdk/terraform 1.0.0 2.836.5 1.0.0 github.com/airbytehq/terraform-provider-airbyte/internal/sdk",
 			ServerList: ServerList,
 		},
 		hooks: hooks.New(),
